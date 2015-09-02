dd41fecb41d0d171bbc670e7d166ab5fe47b49cd
Firstname Lastname <vic@vicdeMacBook-Air.local>
2015-09-03 00:04:14 +0800

    测试
    测试2

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by vic on 15/9/3.
+//  Copyright (c) 2015年 vic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
